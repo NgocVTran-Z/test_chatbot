acfb2c3345c47e7da77976ed77e5069053a77fc3
Ngoc V. Tran <113083475+NgocVTran-Z@users.noreply.github.com>
2024-03-21 06:32:25 +0700

    Update qna.py

--- utils/qna.py
+++ utils/qna.py
@@ -1,9 +1,17 @@
-ans = (
+qnaans = (
     ("tôi muốn mua áo thun", "Bạn vui lòng cho biết kích cỡ và màu áo bạn thích?"),
-    ("có size M màu trắng ko?", "Có, Áo Thun Dry-EX Cổ Tròn Ngắn Tay và AIRism Cotton Áo Thun Kẻ Sọc Cổ Tròn Dáng Rộng "
+    ("có size M màu trắng ko?", "Có, Áo Thun Dry-EX Cổ Tròn Ngắn Tay và AIRism Cotton Áo Thun Kẻ Sọc Cổ Tròn Dáng Rộng"
                                 "đều có size M màu trắng của nam, hiện đang bán tại cửa hàng Quận 1 và Quận 3"),
-    ("cửa hàng Q1 địa chỉ là gì?", "Cửa hàng Quận 1 địa chỉ 17 Lê Thánh Tông"),
-    ("có hot line ko?", "Cửa hàng Quận 1 có số điện thoại là 0901 234 567 89"),
-    ("cửa hàng hiện có đang mở cửa ko?", "Giờ mở cửa của cửa hàng Quận 1 là từ 8h sáng đến 10h đêm thứ 2-7 hàng tuần"),
-    ("cảm ơn", "Cảm ơn quý khách đã ghé thăm")
+    ("mẫu nào đang có chương trình giảm giá?", "Hiện tại Áo Thun Dry-Ex Cổ Tròn Ngắn Tay đang giảm giá còn 293000, "
+                                                "giá gốc là 391000"),
+    ("cửa hàng nào đang bán sản phẩm này?", "Hiện tại cửa hàng Quận 1 đang bán Áo Thun Dry-Ex Cổ Tròn Ngắn Tay này"),
+    ("địa chỉ cửa hàng Q1?", "Cửa hàng Quận 1 địa chỉ 17 Lê Thánh Tông"),
+    ("cửa hàng mở cửa lúc mấy giờ?", "Giờ mở cửa của cửa hàng Quận 1 là từ 8h sáng đến 10h đêm thứ 2-7 hàng tuần"),
+    ("có số hot line ko?", "Cửa hàng Quận 1 có số điện thoại là 090123456789"),
+    ("chính sách đổi trả như thế nào?", "Với sản phẩm này quý khách được quyền đổi sang mẫu khác cùng giá tiền "
+                                        "trong vòng 7 ngày, chú ý sản phẩm không bị hư hỏng"),
+    ("có thanh toán quẹt thẻ không?", "Có, cửa hàng có hỗ trợ thanh toán qua thẻ, tiền mặt hoặc chuyển khoản."),
+    ("cảm ơn", "Cám ơn quý khách, hiện tại cửa hàng đang có chương trình giảm giá khi mua thắt lưng, quý khách "
+               "có nhu cầu mua thêm thắt lưng không?"),
+    ("không, cám ơn", "Xin cám ơn quý khách đã liên hệ hỗ trợ")
 )
